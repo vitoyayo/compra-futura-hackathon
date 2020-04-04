09c7e6c0cad59613433925a2c2b033f1bd5c834a
Elias <elias.vicente@increasecard.com>
2020-04-03 23:21:04 -0300

    Big scraper refactor

--- controllers/scraper.js
+++ controllers/scraper.js
@@ -1,107 +1,136 @@
-require('dotenv').config()
-
+require('dotenv').config();
+const fs = require('fs');
 const puppeteer = require('puppeteer');
 const queue = require('../utils/queue');
 const axios = require('axios');
 
-const launchHeadless = Boolean(Number(process.env.HEADLESS));
+const launchHeadless = true;
 let browser;
 
 const launchBrowser = async () => {
+  browser =
+    browser ||
+    (await puppeteer.launch({
+      args: ['--no-sandbox'],
+      headless: launchHeadless,
+    }));
+  return browser;
+};
 
-  return uppeteer.launch({
-    args: ['--no-sandbox'],
-    headless: launchHeadless,
-  });
+const closeBrowser = async () => {
+  return browser ? await browser.close() : Promise.reject(new Error('Browser is not active'));
 };
 
 const shopListUrl = process.env.URL_SHOPS;
-const shopListSelector = '.biz-card a';
-
+const shopBaseUrl = 'https://comprafutura.com/comercio/';
+// const mainShopSelector = '.card-container';
+const shopInfoSelectors = {
+  shopList: '.biz-card a',
+  shopName: 'h4 b',
+  logo: '.profile-logo',
+  instagramProfile: '.bio-instagram-link',
+  shopDescription: '#root > div > section:nth-child(1) > div > div > div > p',
+  location: '.profile-location',
+  voucherList: '.voucher-card',
+  voucherName: 'h4.text-center',
+  price: 'h3.type--fade',
+  voucherDesciption: 'p.lead',
+};
 
 module.exports = {
-  closeBrowser: async function () {
-    return browser ? await browser.close() : Promise.reject(new Error('Browser is not active'));
-  },
-  launchBrowser: async function() {
-    return browser ||
-        (await puppeteer.launch({
-          args: ['--no-sandbox'],
-          headless: launchHeadless,
-        }));
-  },
-
-
-getShopList: async function() {
-     let browser = await this.launchBrowser();
-     const page = await browser.newPage();
-    // Asumo que si se llamó a la función sin argumentos, es la primera llamada.
+  getShopList: async function () {
+    const page = await browser.newPage();
     await page.goto(shopListUrl, { timeout: 45000, waitUntil: 'load' });
-    await page.waitForSelector(shopListSelector);
+    await page.waitForSelector(shopInfoSelectors.shopList);
 
     return await page.evaluate((selector) => {
       const shops = document.querySelectorAll(selector);
-      console.log(shops)
+      console.log(shops);
       return Array.from(shops).map((elem) => {
         const shopUrlSplited = elem.href.split('/');
         return {
           shopName: elem.querySelector('h4 b').innerText,
           shopId: shopUrlSplited[shopUrlSplited.length - 1],
           createdAt: new Date().toDateString(),
-          updatedAt: new Date().toDateString()
+          updatedAt: new Date().toDateString(),
         };
       });
-    }, shopListSelector);
-    this.closeBrowser();
+    }, shopInfoSelectors.shopList);
   },
 
   shopList: async function () {
-      let list_shops = await this.getShopList();
-      list_shops.forEach(element =>
-        axios.post(`http://${process.env.HOST}:${process.env.PORT}/shops`,{
-        name: element.name ,
-        id: element.id,
-        created_at: element.createdAt ,
-        updated_at: element.updatedAt
-      }).then(function(response){
-        console.log(response)
-      }).catch(function(error){
-        console.log(error)
-        console.log(error)
-      })
-      )
-  }
-
-/*
-  async getShopInfo(shopList, productSelectors) {
+    await launchBrowser();
+    const list_shops = await this.getShopList();
+    const shopsInfo = await this.getShopInfo(list_shops);
+    await closeBrowser();
+    fs.writeFileSync('shop-list.json', JSON.stringify(list_shops));
+    fs.writeFileSync('shops-info.json', JSON.stringify(shopsInfo));
+    list_shops.forEach((element) =>
+      axios
+        .post(`http://${process.env.HOST}:${process.env.PORT}/shops`, {
+          name: element.name,
+          id: element.id,
+          created_at: element.createdAt,
+          updated_at: element.updatedAt,
+        })
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log(error);
+          console.log(error);
+        })
+    );
+  },
+  async getShopInfo(shopList) {
     const shopsInfo = [];
-
-    shopList.forEach((cat) => {
+    shopList.forEach((shop) => {
       shopsInfo.push(
-          queue.addTask(async () => {
-            const page = await browser.newPage();
-            await page.goto(cat.href, { waitUntil: 'domcontentloaded' });
-            const categorieProducts = await page.evaluate((selectors) => {
-              const categorieId = Array.from(document.querySelectorAll(selectors.categorieId)).map((elem) => elem.value),
-                  code = Array.from(document.querySelectorAll(selectors.productId)).map((elem) => elem.value),
-                  name = Array.from(document.querySelectorAll(selectors.name)).map((elem) => elem.innerText.trim()),
-                  price = Array.from(document.querySelectorAll(selectors.price)).map((elem) =>
-                      elem.innerText.match(/\d+,?\d+/) ? Number(elem.innerText.match(/\d+,?\d+/)[0].replace(',', '')) : 0
-                  ),
-                  image = Array.from(document.querySelectorAll(selectors.imgSrc)).map((elem) => elem.src);
-              return code.map((item, index) => {
+        queue.addTask(async () => {
+          const page = await browser.newPage();
+          await page.goto(`${shopBaseUrl}${shop.shopId}`, {
+            waitUntil: 'domcontentloaded',
+          });
+          // Indirect way of detecting content rendering, not very nice.
+          await page.waitForFunction('window.document.body.clientHeight > 600');
+          let shopInfo;
+          try {
+            shopInfo = await page.evaluate(
+              (selectors, shop) => {
+                const logoUrlElem = document.querySelector(selectors.logo),
+                  instagramProfileElem = document.querySelector(selectors.instagramProfile),
+                  shopDescriptionElem = document.querySelector(selectors.shopDescription),
+                  locationElem = document.querySelector(selectors.location);
+                const voucherList = Array.from(document.querySelectorAll(selectors.voucherList)).map((voucherElem) => {
+                  return {
+                    name: voucherElem.querySelector(selectors.voucherName).innerText,
+                    price: Number(voucherElem.querySelector(selectors.price).innerText.replace('$', '')),
+                    voucherDesciption: voucherElem.querySelector(selectors.voucherDesciption).innerText,
+                  };
+                });
                 return {
-                  code: item,
-                  categorieId: categorieId[index],
-                  name: name[index],
-                  price: price[index],
-                  image: image[index],
+                  shopId: shop.shopId,
+                  logoUrl: logoUrlElem ? logoUrlElem.src : null,
+                  instagramProfile: instagramProfileElem ? instagramProfileElem.href : null,
+                  shopDescription: shopDescriptionElem ? shopDescriptionElem.innerText : null,
+                  location: locationElem ? locationElem.innerText : null,
+                  voucherList,
                 };
-              });
-            }, productSelectors);
-            await page.close();
-            return categorieProducts;
-          })
+              },
+              shopInfoSelectors,
+              shop
+            );
+          } catch (e) {
+            shopInfo = {
+              shopName: shop.shopName,
+              shopId: shop.shopId,
+              error: e.message,
+            };
+          }
+          console.log('scraped:', shopInfo.shopName);
+          await page.close();
+          return shopInfo;
+        })
       );
     });
     const products = await Promise.all(shopsInfo);
@@ -109,5 +138,5 @@ getShopList: async function() {
       throw new Error('No products found');
     }
     return products;
-  },*/
+  },
 };
